67b7932caf7c885133781eddcd990ec7da516d95
Jidsx <113401757+Jidsx@users.noreply.github.com>
2024-06-11 17:54:14 -0300

    Delete src directory

--- src/main/java/com/example/autolog/dtos/UserRecordDTO.java
+++
@@ -1,7 +0,0 @@
-package com.example.autolog.dtos;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
-public record UserRecordDTO(@NotBlank String name, @NotBlank String cnpj, @NotBlank String email, @NotBlank String password, @NotNull String phone, @NotNull String nameWorkshop, @NotNull String addressWorkshop) {
-}
